35b64e5ce1edf0475b1ad9c8a8c12e37515cda9e
guedanie <danguerrero93@gmail.com>
2020-04-04 11:16:11 -0500

    first commit

--- checkbook_app_tkinter.py
+++ checkbook_app_tkinter.py
@@ -4,12 +4,12 @@ window = tkinter.Tk()
 
 window.title("GUI")
 
-tkinter.Label(window, text = "~~ Welcome to your personal banking app ~~").pack()
+# tkinter.Label(window, text = "~~ Welcome to your personal banking app ~~").pack()
 
-tkinter.Label(window, text = "Please enter select one of the options below to proceed").pack()
+# tkinter.Label(window, text = "Please enter select one of the options below to proceed").pack()
 
-tkinter.Button(window, text = "Make a deposit").pack()
+tkinter.Button(window, text = "Make a deposit").grid(row=0, column=0)
 
-tkinter.Button(window, text= "Make a widraw").pack()
+tkinter.Button(window, text= "Make a widraw").grid(row=0, column=1)
 
 window.mainloop()
